b78b70047b94a043fd8253ccc0f18940ef220248
Victoria Meng <Victoria Meng>
2016-01-26 20:14:49 -0500

    added 21 bonuses

--- lesson_4/21/21.rb
+++ lesson_4/21/21.rb
@@ -2,16 +2,28 @@ require "yaml"
 MESSAGES = YAML.load_file("21_msgs.yml")
 lang = "en"
 
+TARGET = 21
+DEALER_STAND_VALUE = 16
+
 def say(message)
-  puts "--> #{message}"
+  puts "> #{message}"
 end
 
-def instruct(instructions)
-  puts " --- #{instructions} --- "
+def prompt(lang, message)
+  puts MESSAGES[lang][message]
 end
 
-def enter_name(message)
-  instruct "#{message}"
+def announce(message)
+  puts "\n--- #{message} ---"
+  puts "\n"
+end
+
+def prompt_announce(lang, message)
+  announce MESSAGES[lang][message]
+end
+
+def enter_name(lang, message)
+  prompt_announce(lang, message)
   name = gets.chomp
 end
 
@@ -30,105 +42,114 @@ def card_value(card)
   end
 end
 
-def deal_card(deck, hand)
-  card = deck.delete(deck.sample)
-  hand[:cards].push(card)
-  hand[:total] += card_value(card[0])
-  card
-end
-
 def ace_adjust(hand)
   for card in hand[:cards] do 
-    if card[0] == "Ace(11)" && hand[:total] > 21
+    if card[0] == "Ace(11)" && hand[:total] > TARGET
       card[0] = "Ace(1)"
       hand[:total] -= 10
     end
   end
 end
 
-def announce_card(card)
-  puts "Dealer deals a #{card[0]} of #{card[1]}."
+def deal_card(deck, hand)
+  card = deck.delete(deck.sample)
+  hand[:cards].push(card)
+  hand[:total] += card_value(card[0])
+  ace_adjust(hand)
+  card
+end
+
+def card_name(card)
+  "#{card[0]} of #{card[1]}"
+end
+
+def show_card_dealt(card, message)
+  say "#{message} #{card_name(card)}."
 end
 
 def print_hand(name, hand)
-  cards = hand[:cards].map {|card| "#{card[0]} of #{card[1]}"}.flatten
+  cards = hand[:cards].map {|card| "#{card_name(card)}"}.flatten
   puts "#{name}'s Hand: #{cards} --- Total: #{hand[:total]}"
 end
 
 def show_one_card(hand)
   first_card = hand.first
-  puts "Dealer Shows: '#{first_card[0]} of #{first_card[1]}' --- Value: #{card_value(first_card[0])}"
+  puts <<-SHOW_CARD
+Dealer Shows: #{card_name(first_card)} --- \
+Value: #{card_value(first_card[0])}
+    SHOW_CARD
 end
 
 def blackjack_check(hands)
-  return "blackjack_tie" if hands[:player][:total] == 21 && hands[:dealer][:total] == 21
-  return "blackjack_player" if hands[:player][:total] == 21
-  return "blackjack_dealer" if hands[:dealer][:total] == 21
+  return "blackjack_tie" if hands[:player][:total] == TARGET && hands[:dealer][:total] == TARGET
+  return "blackjack_player" if hands[:player][:total] == TARGET
+  return "blackjack_dealer" if hands[:dealer][:total] == TARGET
   nil
 end
 
-def player_turn(name, deck, hand)
+def player_action(message)
   begin
-    instruct "Type 'H' to hit. Type 'S' to stand."
+    announce(message)
     action = gets.chomp
-  end until action.downcase == "h" || action.downcase == "s"
-
-  case action.downcase
-  when "h"
-    card = deal_card(deck, hand)
-    announce_card(card)
-    ace_adjust(hand)
-    print_hand(name, hand)
-    return "player_bust" if hand[:total] > 21
-    nil
-  when "s"
-    "stand"
-  end
+    action = action.downcase
+  end until %w(h s).include?(action)
+  action
 end
 
-def dealer_turn(name, deck, hand)
-  until hand[:total] > 16
+def player_turn(name, deck, hand, message)
+  begin
+    action = player_action(message["hit_stand"])
+    case action
+    when "s"
+      turn_end = "stand"
+    else
+      card = deal_card(deck, hand)
+      show_card_dealt(card, message["show_card_dealt"])
+      print_hand(name, hand)
+      turn_end = "player_bust" if hand[:total] > TARGET
+    end
+  end until turn_end
+  turn_end
+end
+
+def dealer_turn(name, deck, hand, message)
+  until hand[:total] > DEALER_STAND_VALUE
     card = deal_card(deck, hand)
-    announce_card(card)
-    ace_adjust(hand)
-    print_hand("Dealer", hand)
+    show_card_dealt(card, message)
   end
-  return "dealer_bust" if hand[:total] > 21
+  print_hand("Dealer", hand)
+  return "dealer_bust" if hand[:total] > TARGET
   "stand"
 end 
 
 def compare_hands(hands)
   return "tie" if hands[:player][:total] == hands[:dealer][:total]
-  return "player_win" if hands[:player][:total] > hands[:dealer][:total]
-  return "dealer_win" if hands[:player][:total] < hands[:dealer][:total]
+  return "player_standard_win" if hands[:player][:total] > hands[:dealer][:total]
+  return "dealer_standard_win" if hands[:player][:total] < hands[:dealer][:total]
 end
 
-def announce_winner(name, game_end)
-  case game_end
-  when "blackjack_tie"
-    puts "You both get Blackjack, but dealer wins ties. You lose!"
-  when "blackjack_dealer"
-    puts "Dealer gets Blackjack! You lose!"
-  when "blackjack_player"
-    puts "#{name} gets Blackjack! You win!"
-  when "tie"
-    puts "Dealer wins ties. You lose!"
-  when "player_bust"
-    puts "#{name} busted! You lose!"
-  when "dealer_bust"
-    puts "Dealer busted! You win!"
-  when "player_win"
-    puts "Congratulations, #{name} wins!"
-  when "dealer_win"
-    puts "Sorry, #{name} loses!"
+def print_winner(name, game_end, messages)
+  if messages["player_win"].keys.include?(game_end)
+    announce "#{name} #{messages["player_win"][game_end]}"
+  else
+    announce "#{messages["player_lose"][game_end]}"
   end
+end
+
+def play_again?(lang, message)
+  begin
+    prompt(lang, message)
+    answer = gets.chomp
+    yes_no = answer.downcase
+  end until %w(y n).include?(yes_no)
+  yes_no == "y"
 end
 
 loop do
   eight_decks = []
   eight_decks = new_deck * 8
 
-  name = enter_name(MESSAGES[lang]["enter_name"])
+  name = enter_name(lang, "enter_name")
   hands = {  player: {  cards: [], 
                        total: 0
                     },
@@ -137,35 +158,34 @@ loop do
                     }
           }
 
-  2.times {deal_card(eight_decks, hands[:player])}
-  2.times {deal_card(eight_decks, hands[:dealer])}
+  2.times do
+    deal_card(eight_decks, hands[:player])
+    deal_card(eight_decks, hands[:dealer])
+  end
 
   if blackjack_check(hands)
     game_end = blackjack_check(hands)
   else
     print_hand(name, hands[:player])
     show_one_card(hands[:dealer][:cards])
-    begin
-      player_turn_end = player_turn(name, eight_decks, hands[:player])
-    end until player_turn_end
+    player_turn_end = player_turn(name, eight_decks, hands[:player], MESSAGES[lang])
+
     if player_turn_end == "player_bust"
       game_end = "player_bust"
     else
       print_hand("Dealer", hands[:dealer])
-      dealer_turn_end = dealer_turn("Dealer", eight_decks, hands[:dealer])
+      dealer_turn_end = dealer_turn("Dealer", eight_decks, hands[:dealer], MESSAGES[lang]["show_card_dealt"])
       game_end = "dealer_bust" if dealer_turn_end == "dealer_bust"
       game_end = compare_hands(hands) if dealer_turn_end == "stand"
     end  
   end
 
-  instruct "Final Results"
+  prompt_announce(lang, "final_results")
   print_hand(name, hands[:player])
   print_hand("Dealer", hands[:dealer])
-  announce_winner(name, game_end)
+  print_winner(name, game_end, MESSAGES[lang]["results"])
 
-  puts "Press 'Y' to play again. Press any other key to exit."
-  play_again = gets.chomp
-  break unless play_again.downcase == "y"
+  break unless play_again?(lang, "play_again")
 end
 
-say "Thanks for playing!"
+prompt(lang, "thanks")
