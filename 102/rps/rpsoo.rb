51c2f32763e170d907a03d95a842f1ed52985239
Victoria Meng <Victoria Meng>
2016-05-29 21:19:27 -0400

    added human/computer subclasses

--- 102/rps/rpsoo.rb
+++ 102/rps/rpsoo.rb
@@ -2,80 +2,28 @@ require "yaml"
 
 HANDS = { %w(rock r) => "Rock", %w(paper p) => "Paper", %w(scissors s) => "Scissors" }
 
-WINNING_HANDS = { "Rock" => "Scissors", "Paper" => "Rock", "Scissors" => "Paper" }
+LOSING_HANDS = { "Rock" => "Scissors", "Paper" => "Rock", "Scissors" => "Paper" }
 
 class Player
-  attr_accessor :wins
+  attr_accessor :wins, :hand
+  attr_reader :name
+end
 
-  attr_reader :hand, :name
-
-  @@winner = nil
-
+class Human < Player
   def initialize(name)
     @name = name
     @wins = 0
   end
 
-  def self.compare_hands(human, computer)
-    if human.hand == computer.hand
-      Round.add_tie
-      Round.round_winners << "tie"
-    elsif WINNING_HANDS[human.hand] == computer.hand
-      human.wins += 1
-      Round.round_winners << human.name
-    else
-      computer.wins += 1
-      Round.round_winners << "Computer"
-    end
+  def self.enter_name
+    begin 
+      puts "What's your name?"
+      name = gets.chomp
+    end while name.empty?
+    name
   end
 
-  def self.show_round_winner(human, computer)
-    case Round.round_winners[-1]
-    when "tie"
-      say "You both picked #{human.hand}. It's a tie!"
-    when human.name
-      say "#{human.name} picked #{human.hand}. Computer picked #{computer.hand}. You win!"
-    else
-      say "#{human.name} picked #{human.hand}. Computer picked #{computer.hand}. You lose!"
-    end
-  end
-
-  def self.set_game_winner(human, computer)
-    @@winner = human.name if human.wins > computer.wins
-    @@winner = "Computer" if computer.wins > human.wins
-    @@winner = "tie" if human.wins == computer.wins
-  end
-
-  def self.game_winner
-    @@winner
-  end
-
-  def self.show_game_winner(human, computer)
-    print "--> #{human.name} won #{human.wins} rounds. Computer won #{computer.wins} rounds. "
-    case @@winner
-    when human.name
-      puts "You win!"
-    when "Computer"
-      puts "You lose!"
-    else
-      puts "It's a tie!"
-    end
-  end
-
-  def choose_hand
-    if @name == "Computer"
-      @hand = HANDS.values.sample
-    else
-      input = validate_input
-      match_input(input)
-    end
-  end
-
-  def list_hands
-    HANDS.values.each { |hand| say "Enter #{hand[0]} for #{hand}." }
-  end
-
-  def validate_input
+  def validate_hand
     begin
       list_hands
       input = gets.chomp
@@ -84,8 +32,28 @@ class Player
     input
   end
 
-  def match_input(input)
+  def match_hand(input)
     HANDS.keys.each { |set| @hand = HANDS[set] if set.include?(input) }
+  end
+
+  def choose_hand
+    input = validate_hand
+    match_hand(input)
+  end
+
+  def list_hands
+    HANDS.values.each { |hand| puts "Enter #{hand[0]} for #{hand}." }
+  end
+end
+
+class Computer < Player
+  def initialize
+    @name = "Computer"
+    @wins = 0
+  end
+
+  def choose_hand
+    @hand = HANDS.values.sample
   end
 end
 
@@ -117,55 +85,101 @@ class Round
   def self.five_rounds?
     @@rounds == 5
   end
+
+  def self.next_round?
+    begin
+      puts "Play next round? Press 'Y' for yes. Press 'N' for no."
+      input = gets.chomp
+      input = input.downcase
+    end until %w(y yes n no).include?(input)
+    %w(y yes).include?(input)
+  end
 end
 
-def say(message)
-  puts "--> #{message}"
+class Game
+  attr_accessor :human, :computer
+
+  attr_reader :winner
+
+  def initialize
+    @human = Human.new(Human.enter_name)
+    @computer = Computer.new
+  end
+
+  def show_intro
+    puts "Let's play Rock-Paper-Scissors. Best of 5 wins the game!"
+  end
+
+  def play_round
+    computer.choose_hand
+    human.choose_hand
+    compare_hands
+    show_round_winner
+    show_score
+  end
+
+  def compare_hands
+    if human.hand == computer.hand
+      Round.add_tie
+      Round.round_winners << "tie"
+    elsif LOSING_HANDS[human.hand] == computer.hand
+      human.wins += 1
+      Round.round_winners << human.name
+    else
+      computer.wins += 1
+      Round.round_winners << "Computer"
+    end
+  end
+
+  def show_round_winner
+    case Round.round_winners[-1]
+    when "tie"
+      puts "You both picked #{human.hand}. It's a tie!"
+    when human.name
+      puts "#{human.name} picked #{human.hand}. Computer picked #{computer.hand}. You win!"
+    else
+      puts "#{human.name} picked #{human.hand}. Computer picked #{computer.hand}. You lose!"
+    end
+  end
+
+  def show_score
+    puts "Score - #{human.name}: #{human.wins} - Computer: #{computer.wins} - Ties: #{Round.ties}"
+  end
+
+  def set_game_winner
+    @winner = human.name if human.wins > computer.wins
+    @winner = "Computer" if computer.wins > human.wins
+    @winner = "tie" if human.wins == computer.wins
+  end
+
+  def show_game_winner
+    print "--> #{human.name} won #{human.wins} rounds. Computer won #{computer.wins} rounds. "
+    case winner
+    when human.name
+      puts "You win!"
+    when "Computer"
+      puts "You lose!"
+    else
+      puts "It's a tie!"
+    end
+  end
+
+  def show_bye
+    puts "Thanks for playing!"
+  end
 end
 
-def set_name
-  say "What's your name?"
-  gets.chomp
-end
-
-def show_intro
-  say "Let's play Rock-Paper-Scissors. Best of 5 wins the game!"
-end
-
-def play_round(human, computer)
-  Round.new
-  computer.choose_hand
-  human.choose_hand
-  Player.compare_hands(human, computer)
-  Player.show_round_winner(human, computer)
-  Player.show_score(human, computer)
-end
-
-def show_score(human, computer)
-  say "Rounds Played: #{Round.rounds} - Wins: #{human.wins} - Losses: #{computer.wins} - Ties: #{Round.ties}"
-end
-
-def play_again?
-  begin
-    say "Play next round? Press 'Y' for yes. Press 'N' for no."
-    input = gets.chomp
-    input = input.downcase
-  end until %w(y yes n no).include?(input)
-  %w(y yes).include?(input)
-end
-
-def show_bye
-  say "Thanks for playing!"
-end
-
-human = Player.new(set_name)
-computer = Player.new("Computer")
-show_intro
+game = Game.new
+game.show_intro
 
 begin
-  play_round(human, computer)
-end until Round.five_rounds? || !play_again?
+  Round.new
+  game.play_round
+end until Round.five_rounds? || !Round.next_round?
 
-Player.set_game_winner(human, computer)
-Player.show_game_winner(human, computer) if Player.game_winner
-show_bye
+if Round.five_rounds?
+  game.set_game_winner
+  game.show_game_winner
+end
+
+game.show_bye
